3b74e316eb713a32e5e69d81fe9406b9ce0fa8e4
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2021-10-18 11:56:54 +0100

    adding last updates

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,4 +33,39 @@ class AuthController extends Controller
 
         return response($response, 201);
     }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' =>'required|string'
+        ]);
+
+        //  check email
+        $user = User::where('email', $fields['email'])->first();
+
+        //  check password
+        if(!$user || !Hash::check($fields['password'],$user->password)){
+            return response([
+                'message' => 'something wrong'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response =
+        [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout (Request $request){
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'logged out'
+        ];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\ProductController;
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products' ,[ProductController::class, 'index']);
 Route::get('/products/{id}' ,[ProductController::class, 'show']);
 Route::get('/products/search/{name}' ,[ProductController::class, 'search']);
@@ -30,6 +31,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products' ,[ProductController::class, 'store']);
     Route::put('/products/{id}' ,[ProductController::class, 'update']);
     Route::delete('/products/{id}' ,[ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
